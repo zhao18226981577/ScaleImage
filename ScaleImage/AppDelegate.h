0a92fb0ee80ae24ddfb67ec2bfa0b6a2a5f5354b
zhao18226981577 <2789304360@qq.com>
2017-05-25 11:44:37 +0800

    Initial Commit

---
+++ ScaleImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScaleImage
+//
+//  Created by 精智 on 17/5/25.
+//  Copyright © 2017年 精智. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
